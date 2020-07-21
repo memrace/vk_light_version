1467b16baa2a5d6ef7d9a8ce9d1f1668fed8a25a
robbier-sozo <sozonovalexander13@gmail.com>
2020-07-22 00:17:33 +0300

    NavigationView Listener

--- app/src/main/java/com/android/example/vk_light_version/StartActivity.kt
+++ app/src/main/java/com/android/example/vk_light_version/StartActivity.kt
@@ -1,15 +1,21 @@
 package com.android.example.vk_light_version
 
-import android.content.res.Resources
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.android.example.vk_light_version.databinding.ActivityStartBinding
 import com.android.example.vk_light_version.fragments.PageAdapter
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_start.*
 import kotlinx.android.synthetic.main.toolbar_main.view.*
 
-class StartActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
     private lateinit var viewBinding:ActivityStartBinding
 
 
@@ -24,7 +30,8 @@ class StartActivity : AppCompatActivity() {
         settingUpViewPager()
         // Toggle for Drawer layout
         settingDrawerToggle()
-
+        // NavigationListener
+        settingNavigationListener()
 
     }
 
@@ -38,8 +45,23 @@ class StartActivity : AppCompatActivity() {
         val toggle = ActionBarDrawerToggle(this,drawer_layout,incl_toolbar.eternal_toolbar,R.string.nav_open_drawer,R.string.nav_close_drawer)
         drawer_layout.addDrawerListener(toggle)
         toggle.drawerArrowDrawable.color = resources.getColor(R.color.colorAccent)
+        toggle.drawerArrowDrawable.gapSize = 15f
         toggle.syncState()
     }
+
+    private fun settingNavigationListener() {
+        viewBinding.navView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            0 -> startActivity(Intent(this, NewsFeed::class.java))
+            else -> startActivity(Intent(this, NewsFeed::class.java))
+        }
+        return item.itemId != null
+    }
+
 
 }
 
